d76a158340ff077cca70f2f400b3cd71e1f7e858
plutbart <plutabartek@gmail.com>
2019-06-30 22:53:28 +0200

    Initial Commit

---
+++ Cwift/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Cwift
+//
+//  Created by Bartłomiej Pluta on 30/06/2019.
+//  Copyright © 2019 Bartłomiej Pluta. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
